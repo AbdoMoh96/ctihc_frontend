6a2facb7de5cce2d5e1798543e6850e3cd394abc
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-04 18:13:53 +0300

    working on animation fix

--- app/Components/Layout/Nav/Nav.tsx
+++ app/Components/Layout/Nav/Nav.tsx
@@ -5,25 +5,32 @@ interface propTypes {}
 
 const Nav: React.FC<propTypes> = () => {
 
-    const [navState, setNaveState] = useState<string>('');
+    const [navState, setNavState] = useState<string>('');
 
     useEffect(() => {
+        // handel scroll event animation using useState and native event listener
         document.addEventListener('scroll', () => {
            if (window.scrollY >= 150 && window.scrollY < 200){
-               setNaveState('-top-20 opacity-0');
+               setNavState('-top-20 opacity-0');
            }else if(window.scrollY >= 200){
-               setNaveState('fixed top-0 opacity-1 bg-red-600 w-screen');
+               setNavState('fixed top-0 opacity-1 bg-blue-600 w-screen text-white');
            }
            else{
-               setNaveState('');
+               setNavState('');
            }
         })
+        // trigger a scroll event for when the page is reloaded or navigated not from the beginning
+        window.scrollTo(window.scrollX, window.scrollY - 1);
+        window.scrollTo(window.scrollX, window.scrollY + 1);
     }, []);
 
     return (
         <nav className="h-20">
-            <div className={`h-20 transition-all duration-500 ease-in-out bg-red-600 ${navState}`}>
-              
+            <div className={`h-20 transition-all duration-500 ease-in-out flex justify-between items-center p-3 ${navState}`}>
+                   <h2>Logo</h2>
+                   <div>
+
+                   </div>
             </div>
         </nav>
     )
