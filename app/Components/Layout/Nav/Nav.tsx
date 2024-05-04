62545109aff017f10b039d2680d484b11089d23e
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-04 16:13:52 +0300

    added nav state

--- app/Components/Layout/Nav/Nav.tsx
+++ app/Components/Layout/Nav/Nav.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+'use client'
+import React, {useEffect, useState} from 'react';
 
 interface propTypes {}
 
 const Nav: React.FC<propTypes> = () => {
 
-    return (
-        <nav>
+    const [navState, setNaveState] = useState<string>('');
 
+    useEffect(() => {
+        document.addEventListener('scroll', () => {
+           if (window.scrollY >= 5){
+               setNaveState('bg-red-600');
+           }else{
+               setNaveState('');
+           }
+        })
+    }, []);
+
+    return (
+        <nav className="bg-blue-500 h-20">
+            <div className={`h-20 ${navState}`}>
+
+            </div>
         </nav>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <main>
+    <main className="h-screen">
       
     </main>
   );
