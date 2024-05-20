d7e048a376be572aa19343ba582fe03b8719c184
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-20 19:57:42 +0300

    added new fonts and mobile menu basic style

--- app/Components/Layout/Nav/Components/DropDown/DropDown.tsx
+++ app/Components/Layout/Nav/Components/DropDown/DropDown.tsx
@@ -15,7 +15,7 @@ const DropDown : React.FC<propTypes> = ({children, title}) => {
         onClick={() => setShowDropDown(state => !state)}>
         {title}
         <div
-            className={`lg:invisible text-nowrap ${showDropDown ? 'max-h-60' : 'max-h-0'} lg:max-h-max overflow-hidden w-full bg-white lg:drop-shadow-lg lg:rounded transition-all duration-700 lg:duration-200 ease-in-out lg:absolute mt-3 z-10 top-full lg:w-auto lg:py-2 text-black pl-5 pr-2 flex flex-col items-end gap-2 right-0 lg:opacity-0 group-hover:visible lg:group-hover:mt-1 group-hover:opacity-100`}>
+            className={`lg:invisible bg-zinc-800 text-white text-nowrap ${showDropDown ? 'max-h-96' : 'max-h-0'} lg:max-h-max overflow-hidden w-full lg:drop-shadow-lg lg:rounded transition-all duration-700 lg:duration-200 ease-in-out lg:absolute mt-3 z-10 top-full lg:w-auto lg:py-2 text-black pl-5 pr-2 flex flex-col items-end gap-2 right-0 lg:opacity-0 group-hover:visible lg:group-hover:mt-1 group-hover:opacity-100`}>
             {children}
         </div>
     </div>
--- app/Components/Layout/Nav/Components/NavLink/NavLink.tsx
+++ app/Components/Layout/Nav/Components/NavLink/NavLink.tsx
@@ -10,7 +10,7 @@ interface propTypes {
 
 const NavLink : React.FC<propTypes> = ({text, href = '#', className = '', onClick}) => {
 
-    return <Link onClick={onClick} href={href} className={`lg:h-full border-b-2 transition-all duration-500 ease-in-out border-transparent hover:border-amber-500 flex items-center hover:text-amber-500 ${className}`}>
+    return <Link onClick={onClick} href={href} className={`lg:h-full transition-all inline-block mx-auto lg:mx-0 duration-500 ease-in-out pb-1 text-center lg:pb-0 border-b lg:border-b-2 w-11/12 lg:w-auto border-gray-100 lg:border-transparent hover:border-amber-500 lg:flex lg:items-center hover:text-amber-500 ${className}`}>
         {text}
     </Link>
 }
