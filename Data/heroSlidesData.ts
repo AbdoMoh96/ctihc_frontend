f61d90cc371ffd948133c9709d003de8d5bc2cb7
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-21 01:23:37 +0300

    added hero swiper data in temp Data

---
+++ Data/heroSlidesData.ts
@@ -0,0 +1,19 @@
+const slides = [
+    {
+        image: 'https://images.unsplash.com/photo-1714837003223-5144b6e082cb?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide 1 Title',
+        description: 'Slide 1 Description',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?q=80&w=2542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide 2 Title',
+        description: 'Slide 2 Description',
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        title: 'Slide 3 Title',
+        description: 'Slide 3 Description',
+    },
+];
+
+export default slides;
