efb8f57e633522a43562eb9e3c5aefbd449b3964
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-21 23:02:34 +0300

    added services section

--- app/page.tsx
+++ app/page.tsx
@@ -29,15 +29,109 @@ export default function HomePage() {
                       <h1 className='text-3xl font-roboto font-semibold text-center'>latest news</h1>
                       <div className='w-full py-2 grid grid-cols-1 lg:grid-cols-3 gap-10'>
                           <div
-                              className='h-72 rounded-2xl bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'></div>
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
                           <div
-                              className='h-72 rounded-2xl bg-[url("https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e")] bg-cover'></div>
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
                           <div
-                              className='h-72 rounded-2xl bg-[url("https://images.unsplash.com/photo-1553432172-f37667f5ed15")] bg-cover'></div>
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1553432172-f37667f5ed15")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
                       </div>
                       <button
                           className='font-roboto transition-all ease-in-out duration-300 border border-black rounded-full py-1 px-2 hover:bg-zinc-800 hover:text-white'>
                           show all news
+                      </button>
+                  </Container>
+              </section>
+
+              <section className="py-2 mt-28" id='services-section'>
+                  <Container className='gap-7 lg:gap-5 flex flex-col justify-center items-center'>
+                      <h1 className='text-3xl font-roboto font-semibold text-center'>our services</h1>
+                      <div className='w-full py-2 grid grid-cols-1 lg:grid-cols-4 gap-10'>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1553432172-f37667f5ed15")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                          <div
+                              className='h-72 rounded-2xl cursor-pointer bg-[url("https://images.unsplash.com/photo-1491036775913-3fbc5c455842")] bg-cover'>
+                              <div
+                                  className=" transition-all ease-in-out duration-300 w-full h-full flex flex-col-reverse rounded-2xl p-3 bg-zinc-800 bg-opacity-0 hover:bg-opacity-30">
+                                  <h4 className='text-white font-roboto'>Lorem ipsum dolor sit amet, consectetur
+                                      adipisicing elit. Eius, repudiandae!</h4>
+                              </div>
+                          </div>
+                      </div>
+                      <button
+                          className='font-roboto transition-all ease-in-out duration-300 border border-black rounded-full py-1 px-2 hover:bg-zinc-800 hover:text-white'>
+                          show all services
                       </button>
                   </Container>
               </section>
