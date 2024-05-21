359242a9045f5afefbf8dc2045103394682aa305
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-21 21:53:54 +0300

    added about section mobile support and removed border bottom for mobile menu

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function HomePage() {
   return (
       <>
         <main>
-              <section className='h-[70vh]' id="hero section">
+              <section className='h-[70vh]' id="hero-section">
                    <HeroSlider slides={slides}/>
               </section>
-              <section className="py-2 lg:mt-6" id='about section'>
+              <section className="py-2 lg:mt-6" id='about-section'>
                   <Container className='h-full gap-7 lg:gap-5 flex flex-col justify-center items-center'>
                       <h1 className='text-2xl font-roboto font-semibold text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
                       <p className='text-xl font-roboto font-light text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi dolorum eum non odit, quas quo sapiente similique vero voluptas.</p>
@@ -18,10 +18,6 @@ export default function HomePage() {
                           know more
                       </button>
                   </Container>
-              </section>
-
-              <section className="py-2" id='news section'>
-
               </section>
         </main>
       </>
