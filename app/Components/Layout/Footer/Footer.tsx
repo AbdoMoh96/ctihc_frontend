ce1f54862e8d0016a6a143fac8b19b45521c4c02
Abdelrahman Mohammed <armohamed@beltoneholding.com>
2024-05-22 15:43:12 +0300

    added footer mobile support

--- app/Components/Layout/Footer/Footer.tsx
+++ app/Components/Layout/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from "@/app/Components/Layout/Container";
 import Image from 'next/image';
+import Link from "next/link";
 
 
 interface propTypes {}
@@ -10,7 +11,7 @@ const Footer: React.FC<propTypes> = () => {
     return (
         <footer className="py-2 mt-28 bg-zinc-800">
             <Container className='gap-7 lg:gap-5 flex flex-col justify-center items-center'>
-                <div className='w-full py-2 grid grid-cols-1 lg:grid-cols-4 gap-10'>
+                <div className='w-full py-2 grid grid-cols-1 lg:grid-cols-4 gap-16'>
                     <div className='py-2 flex flex-col gap-2'>
                         <div className='flex flex-row items-center gap-2'>
                             <Image src='/assets/images/ctihc_logo.png' alt='ctihc_logo' height={100} width={80}/>
@@ -24,9 +25,45 @@ const Footer: React.FC<propTypes> = () => {
 
                         </div>
                     </div>
-                    <div className='py-2'></div>
-                    <div className='py-2'></div>
-                    <div className='py-2'></div>
+                    <div className='py-2 flex flex-col items-center lg:items-start text-xl lg:text-base'>
+                        <h1 className='font-roboto mb-2 font-semibold text-white capitalize'>browse</h1>
+                        <div className='flex flex-col font-roboto text-gray-400 items-center gap-2 lg:items-start'>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>home</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>about</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>contact us</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>sections</Link>
+                        </div>
+                    </div>
+                    <div className='py-2 flex flex-col items-center lg:items-start text-xl lg:text-base'>
+                        <h1 className='font-roboto mb-2 font-semibold text-white capitalize'>browse</h1>
+                        <div className='flex flex-col font-roboto text-gray-400 items-center gap-2 lg:items-start'>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>home</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>about</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>contact us</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>sections</Link>
+                        </div>
+                    </div>
+                    <div className='py-2 flex flex-col items-center lg:items-start text-xl lg:text-base'>
+                        <h1 className='font-roboto mb-2 font-semibold text-white capitalize'>browse</h1>
+                        <div className='flex flex-col font-roboto text-gray-400 items-center gap-2 lg:items-start'>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>home</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>about</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>contact us</Link>
+                            <Link className='transition-all duration-500 ease-in-out hover:text-amber-500 w-fit'
+                                  href='#'>sections</Link>
+                        </div>
+                    </div>
                 </div>
             </Container>
         </footer>
