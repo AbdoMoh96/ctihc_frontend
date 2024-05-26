8daaf12c98bc0acb315c55501b7622fc11135883
Abdelrahman Mohammed <armohamed@beltoneholding.com>
2024-05-26 16:18:13 +0300

    added navLink children

--- app/[locale]/Components/Layout/Nav/Components/NavLink/NavLink.tsx
+++ app/[locale]/Components/Layout/Nav/Components/NavLink/NavLink.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import Link from "next/link";
 
 interface propTypes {
     className? :string;
-    text : string;
+    text? : string;
     href? : string;
+    children?: ReactNode;
     onClick? : () => void;
 }
 
-const NavLink : React.FC<propTypes> = ({text, href = '#', className = '', onClick}) => {
+const NavLink : React.FC<propTypes> = ({text,children, href = '#', className = '', onClick}) => {
 
     return <Link onClick={onClick} href={href} className={`lg:h-full transition-all inline-block mx-auto lg:mx-0 duration-500 ease-in-out pb-1 text-center lg:pb-0 border-b-transparent lg:border-b-2 w-11/12 lg:w-auto border-gray-100 lg:border-transparent hover:border-amber-500 lg:flex lg:items-center hover:text-amber-500 ${className}`}>
-        {text}
+        {text ?? children}
     </Link>
 }
 
