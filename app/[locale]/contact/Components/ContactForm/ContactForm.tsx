1cd09d09549fbf2054335ae7e45044ec07cd2951
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-05-29 11:58:50 +0300

    added contact page

---
+++ app/[locale]/contact/Components/ContactForm/ContactForm.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React from 'react';
+import { Input } from 'antd';
+import {useTranslations} from "next-intl";
+const { TextArea } = Input;
+
+interface propTypes {}
+
+const ContactForm : React.FC<propTypes> = () => {
+
+    const lang = useTranslations('contactPage');
+
+    return <div
+        className='flex flex-col justify-center items-center gap-6 w-full h-full lg:w-6/12 rounded-t-lg lg:rounded-t-none lg:ltr:rounded-l-lg lg:rtl:rounded-r-lg'>
+        <div className='w-11/12'>
+            <label className='block mb-1'>{lang('full_name')}</label>
+            <Input className='w-full' placeholder={lang('full_name')} />
+        </div>
+        <div className='w-11/12'>
+            <label className='block mb-1'>{lang('phone')}</label>
+            <Input className='w-full' placeholder={lang('phone')}/>
+        </div>
+        <div className='w-11/12'>
+            <label className='block mb-1'>{lang('email')}</label>
+            <Input className='w-full' placeholder={lang('email')} />
+        </div>
+        <div className='w-11/12'>
+            <label className='block mb-1'>{lang('subject')}</label>
+            <Input className='w-full' placeholder={lang('subject')} />
+        </div>
+        <div className='w-11/12'>
+            <label className='block mb-1'>{lang('message')}</label>
+            <TextArea className='w-full' placeholder={lang('message')} rows={4}/>
+        </div>
+    </div>
+}
+
+export default ContactForm;
--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -3,6 +3,11 @@ import Container from "@/app/[locale]/Components/Layout/Container";
 import AnimatedSection from "@/Components/Animation/AnimatedSection";
 import {useTranslations} from "next-intl";
 import {unstable_setRequestLocale} from 'next-intl/server';
+import { CiLocationOn } from "react-icons/ci";
+import { LuPhone } from "react-icons/lu";
+import { MdOutlineFax } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
+import ContactForm from "@/app/[locale]/contact/Components/ContactForm";
 import Data from "@/Data/data";
 
 interface ContactPageProps {
@@ -21,14 +26,25 @@ export default function ContactPage({params: {locale}} : ContactPageProps) {
             <main>
 
 
-                <AnimatedSection className="py-2 h-[50vh]" id='contact-section'>
+                <AnimatedSection className="py-2 my-2 lg:my-20 lg:h-[50vh]" id='contact-section'>
                     <Container className='h-full flex flex-col justify-center items-center'>
-                        <div className='flex flex-col lg:flex-row w-full lg:w-9/12 h-72 rounded-lg shadow-2xl'>
-                            <div className='flex flex-col lg:w-6/12 h-72 bg-zinc-700 rounded-t-lg lg:rounded-t-none lg:ltr:rounded-l-lg lg:rtl:rounded-r-lg'>
-
-                            </div>
-                            <div className='flex flex-col lg:w-6/12 h-72 bg-cyan-500 rounded-b-lg lg:rounded-b-none lg:ltr:rounded-r-lg lg:rtl:rounded-l-lg'>
-
+                        <div className='flex flex-col gap-7 lg:gap-2 lg:flex-row p-5 w-full lg:w-9/12 rounded-lg shadow-2xl font-roboto rtl:font-cairo'>
+                            <ContactForm/>
+                            <div
+                                className='flex flex-col justify-between items-center gap-6 w-full h-full lg:w-6/12 rounded-t-lg lg:rounded-t-none lg:ltr:rounded-l-lg lg:rtl:rounded-r-lg'>
+                                <h1 className='border-b-2 pb-2 border-amber-500'>{lang('contact_us')}</h1>
+                                <div className='flex justify-center items-center gap-5 border-2 border-gray-400 transition-all w-8/12 duration-300 ease-in-out p-5 rounded-lg hover:border-amber-500 hover:text-amber-500'>
+                                    <CiLocationOn size={30}/>{lang('address')}
+                                </div>
+                                <div className='flex justify-center items-center gap-5 border-2 border-gray-400 transition-all w-8/12 duration-300 ease-in-out p-5 rounded-lg hover:border-amber-500 hover:text-amber-500'>
+                                    <LuPhone size={30}/> 23905153-23953447(202)
+                                </div>
+                                <div className='flex justify-center items-center gap-5 border-2 border-gray-400 transition-all w-8/12 duration-300 ease-in-out p-5 rounded-lg hover:border-amber-500 hover:text-amber-500'>
+                                    <MdOutlineFax size={30}/>23903235-23955922(202)
+                                </div>
+                                <div className='flex justify-center items-center gap-5 border-2 border-gray-400 transition-all w-8/12 duration-300 ease-in-out p-5 rounded-lg hover:border-amber-500 hover:text-amber-500'>
+                                    <MdOutlineEmail size={30}/>contact@ctihc.com
+                                </div>
                             </div>
                         </div>
                     </Container>
@@ -37,7 +53,7 @@ export default function ContactPage({params: {locale}} : ContactPageProps) {
                 <iframe
                     src={Data.companyMapsLocation}
                     loading="lazy"
-                    className="border-0 w-full mt-20 h-72"
+                    className="border-0 w-full h-72"
                     referrerPolicy="no-referrer-when-downgrade"></iframe>
             </main>
         </>
