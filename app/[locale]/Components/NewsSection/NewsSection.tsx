57a5cb7f667f176639efd4b9157bac196909d11b
Abdelrahman Mohammed <abdelrahman.mohamed@paysky.io>
2024-09-08 16:24:10 +0300

    added news item navigate link tag with no navigation

--- app/[locale]/Components/NewsSection/NewsSection.tsx
+++ app/[locale]/Components/NewsSection/NewsSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useTranslations} from "next-intl";
+import {Link} from '@/helpers/navigation'
 import config from '@/helpers/config.helper';
 import Container from "@/app/[locale]/Components/Layout/Container";
 import AnimatedSection from "@/Components/Animation/AnimatedSection";
@@ -36,13 +37,14 @@ const NewsSection : React.FC<propTypes> = async ({locale}) => {
             <div className='w-full py-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10'>
 
                 {news?.map((newsItem: any) => {
-                    return <div
+                    return <Link
+                        href={'/news'}
                         key={newsItem.id}
                         className={`transition-all group relative ease-in-out duration-300 w-full flex flex-col-reverse  p-3 h-72 rounded-2xl cursor-pointer bg-cover`} style={{ backgroundImage: "url(" + `${config.AppStorage}/${newsItem.thumbnail}` + ")" }}>
                         <div
                             className='transition-all ease-in-out duration-300 absolute top-0 z-10 left-0 w-full h-full rounded-2xl opacity-0 group-hover:opacity-50 bg-gradient-to-b from-transparent to-gray-950'/>
                         <h4 className='text-white font-roboto rtl:font-cairo z-20'>{newsItem.title}</h4>
-                    </div>
+                    </Link>
                 })}
 
             </div>
