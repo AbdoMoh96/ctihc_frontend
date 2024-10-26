411719ef2bc1aefa3e7545453e3c01571304b681
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-10-26 21:31:00 +0300

    added s3 storage domain

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "landing.ctihc.com",
       },
+      {
+        protocol: "https",
+        hostname: "storage-api.ctihc.com",
+      },
     ],
   },
 };
